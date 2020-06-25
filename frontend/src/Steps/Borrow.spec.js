90321cb839c332dc297c7eb29f5db9ebc02730e3
Troels Ugilt Jensen <tujensen@gmail.com>
2020-06-25 14:12:07 +0200

    BIB-480: Added tests

---
+++ frontend/src/Steps/Borrow.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Borrow from './Borrow';
+import { shallow } from 'enzyme';
+
+it('renders without crashing without machineState', () => {
+    shallow(<Borrow />);
+});
+
+it('renders without crashing', () => {
+    const machineState = {
+        user: {
+            name: 'TestName'
+        },
+        materials: []
+    };
+    shallow(<Borrow machineState={machineState} />);
+});
+
+it('renders the name of the user', () => {
+    const machineState = {
+        user: {
+            name: 'TestName'
+        },
+        materials: []
+    };
+
+    const wrapper = shallow(<Borrow machineState={machineState} />);
+
+    expect(wrapper).toContainReact(<p>Hej TestName</p>);
+});
+
