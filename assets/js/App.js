ee74fcd7c7740a37905378f6aa35898cf3b02d3d
Sine Jespersen <sinejespersen@Sines-MacBook-Pro-2.local>
2020-07-20 09:23:13 +0200

    BIB-431  changed the setup because i could not get the other to work

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,139 +1,176 @@
-import React, { Component } from 'react';
-import './App.css';
-import socketIOClient from 'socket.io-client';
-import Initial from './Steps/Initial';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Alert } from 'react-bootstrap';
-import ScanLogin from './Steps/ScanLogin';
-import Borrow from './Steps/Borrow';
-import ActionFaker from './actionFaker';
+import React, { useState } from "react";
+import socketIOClient from "socket.io-client";
+import Initial from "./Steps/Initial";
+import { Alert } from "react-bootstrap";
+import Login from "./Steps/login";
+import Status from "./Steps/status";
+import Handin from "./Steps/handin";
+import ActionFaker from "./actionFaker";
+import Borrow from "./Steps/borrow";
+import NavBar from "./Steps/components/navbar";
+import MachineStateContext from "./context/machineStateContext";
 
 // @TODO: Rewrite as functional component.
-class App extends Component {
-    constructor(props) {
-        super(props);
+function App() {
+    // constructor(props) {
+    //     super(props);
 
-        const urlParams = new URLSearchParams(window.location.search);
-        const token = urlParams.get('token');
+    //     const urlParams = new URLSearchParams(window.location.search);
+    //     const token = urlParams.get("token");
 
-        this.socket = null;
+    //     this.socket = null;
 
-        this.state = {
-            fake: true,
-            token: token,
-            machineState: {},
-            // @TODO: Make configurable.
-            endpoint: 'http://bibbox-website.local.itkdev.dk:8010'
-        };
+    //     this.state = {
+    //         fake: true,
+    //         token: token,
+    //         machineState: {},
+    //         // @TODO: Make configurable.
+    //         endpoint: "http://bibbox-website.local.itkdev.dk:8010",
+    //     };
 
-        this.handleAction = this.handleAction.bind(this);
-        this.handleReset = this.handleReset.bind(this);
-        this.setMachineState = this.setMachineState.bind(this);
-        this.getMachineState = this.getMachineState.bind(this);
-    }
+    //     this.handleAction = this.handleAction.bind(this);
+    //     this.handleReset = this.handleReset.bind(this);
+    //     this.setMachineState = this.setMachineState.bind(this);
+    //     this.getMachineState = this.getMachineState.bind(this);
+    // }
 
-    setMachineState(data) {
-        this.setState({ machineState: data }, () => {
-            console.log('UpdateState', this.state.machineState);
-        });
-    }
+    // setMachineState(data) {
+    //     this.setState({ machineState: data }, () => {
+    //         console.log("UpdateState", this.state.machineState);
+    //     });
+    // }
 
-    getMachineState() {
-        return this.state.machineState;
-    }
+    // getMachineState() {
+    //     return this.state.machineState;
+    // }
 
-    componentDidMount() {
-        const { endpoint } = this.state;
-        const { fake } = this.state;
+    // componentDidMount() {
+    //     const { endpoint } = this.state;
+    //     const { fake } = this.state;
 
-        if (!fake) {
-            const socket = socketIOClient(endpoint, {
-                transports: ['websocket', 'polling']
-            });
-            this.socket = socket;
-            socket.on('UpdateState', data => {
-                this.setMachineState(data);
-            });
-            // Ready
-            socket.emit('ClientReady', {
-                token: this.state.token
-            });
+    //     if (!fake) {
+    //         const socket = socketIOClient(endpoint, {
+    //             transports: ["websocket", "polling"],
+    //         });
+    //         this.socket = socket;
+    //         socket.on("UpdateState", (data) => {
+    //             this.setMachineState(data);
+    //         });
+    //         // Ready
+    //         socket.emit("ClientReady", {
+    //             token: this.state.token,
+    //         });
+    //     } else {
+    //         console.log("Running with fake content.");
+
+    //         this.actionFaker = new ActionFaker(
+    //             this.getMachineState,
+    //             this.setMachineState
+    //         );
+
+    //         this.setState({
+    //             machineState: {
+    //                 step: "initial",
+    //             },
+    //         });
+    //     }
+    // }
+
+    // function handleAction(action, data) {
+    //     console.log("handleAction", action, data);
+
+    //     const { fake } = this.state;
+
+    //     if (!fake) {
+    //         this.socket.emit("ClientEvent", {
+    //             name: "Action",
+    //             token: this.state.token,
+    //             action: action,
+    //             data: data,
+    //         });
+    //     } else {
+    //         this.actionFaker.handleAction(action, data);
+    //     }
+    // }
+
+    // handleReset() {
+    //     console.log("handleReset");
+
+    //     const { fake } = this.state;
+
+    //     if (!fake) {
+    //         this.socket.emit("ClientEvent", {
+    //             name: "Reset",
+    //             token: this.state.token,
+    //         });
+    //     } else {
+    //         this.actionFaker.handleReset();
+    //     }
+    // }
+
+    function renderStep(step, machineState) {
+        if (step === "initial") {
+            return <Initial />;
+        }
+        if (loggedIn) {
+            switch (step) {
+                case "chooseLogin":
+                    return <div>@TODO: chooseLogin</div>;
+                case "borrow":
+                    return <Borrow />;
+                case "handing":
+                    return (
+                        <Handin
+                            machineState={machineState}
+                            actionHandler={this.handleAction}
+                            handleReset={this.handleReset}
+                        />
+                    );
+                case "status":
+                    return (
+                        <Status
+                            machineState={machineState}
+                            actionHandler={this.handleAction}
+                            handleReset={this.handleReset}
+                        />
+                    );
+                default:
+                    return (
+                        <div
+                            className={"app-default"}
+                            style={{ textAlign: "center" }}
+                        >
+                            <Alert variant={"warning"}>Please wait...</Alert>
+                        </div>
+                    );
+            }
         } else {
-            console.log('Running with fake content.');
-
-            this.actionFaker = new ActionFaker(this.getMachineState, this.setMachineState);
-
-            this.setState({
-                machineState: {
-                    step: 'initial'
-                }
-            });
+            return <Login></Login>;
         }
     }
 
-    handleAction(action, data) {
-        console.log('handleAction', action, data);
-
-        const { fake } = this.state;
-
-        if (!fake) {
-            this.socket.emit('ClientEvent', {
-                name: 'Action',
-                token: this.state.token,
-                action: action,
-                data: data
-            });
-        } else {
-            this.actionFaker.handleAction(action, data);
-        }
-    }
-
-    handleReset() {
-        console.log('handleReset');
-
-        const { fake } = this.state;
-
-        if (!fake) {
-            this.socket.emit('ClientEvent', {
-                name: 'Reset',
-                token: this.state.token
-            });
-        } else {
-            this.actionFaker.handleReset();
-        }
-    }
-
-    renderStep(step, machineState) {
-        switch (step) {
-        case 'initial':
-            return <Initial machineState={machineState} actionHandler={this.handleAction} handleReset={this.handleReset} />;
-        case 'chooseLogin':
-            return <div>@TODO: chooseLogin</div>;
-        case 'loginScan':
-            return <ScanLogin machineState={machineState} actionHandler={this.handleAction} handleReset={this.handleReset} />;
-        case 'borrow':
-            return <Borrow machineState={machineState} actionHandler={this.handleAction} handleReset={this.handleReset} />;
-        default:
-            return (
-                <div className={'app-default'}
-                    style={{ textAlign: 'center' }}>
-                    <Alert variant={'warning'}>
-                            Please wait...
-                    </Alert>
+    const [machineState, setMachineState] = useState();
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [step, setStep] = useState("initial");
+    const [username, setUsername] = useState();
+    const [loginConfig, setLoginConfig] = useState("type");
+    const store = {
+        machineState: { get: machineState, set: setMachineState },
+        step: { get: step, set: setStep },
+        loggedIn: { get: loggedIn, set: setLoggedIn },
+        username: { get: username, set: setUsername },
+        loginConfig: { get: loginConfig },
+    };
+    return (
+        <>
+            <MachineStateContext.Provider value={store}>
+                <NavBar></NavBar>
+                <div className="container">
+                    {renderStep(step, machineState)}
                 </div>
-            );
-        }
-    }
-
-    render() {
-        const { machineState } = this.state;
-
-        return (
-            <div className={'app-container'}>
-                {this.renderStep(machineState.step, machineState)}
-            </div>
-        );
-    }
+            </MachineStateContext.Provider>
+        </>
+    );
 }
 
 export default App;
---
+++ assets/js/context/machineStateContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const MachineStateContext = createContext({});
+
+export default MachineStateContext;
