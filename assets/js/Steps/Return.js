73bd68f5ca7154720e8597864724624b6c27d17f
Troels Ugilt Jensen <tujensen@gmail.com>
2020-07-28 10:33:40 +0200

    BIB-390: Started return flow

---
+++ assets/js/Steps/Return.js
@@ -0,0 +1,106 @@
+import React, { useEffect } from 'react';
+import { Container, Row, Col, Alert, Table, Button } from 'react-bootstrap';
+import BarcodeScanner from './BarcodeScanner';
+import PropTypes from 'prop-types';
+
+/**
+ * Return component.
+ *
+ * Supplies a page for borrowing materials.
+ *
+ * @param props
+ * @return {*}
+ * @constructor
+ */
+function Return(props) {
+    const { actionHandler, handleReset } = props;
+
+    useEffect(() => {
+        console.log('use effect');
+
+        const barcodeScanner = new BarcodeScanner(400);
+
+        const barcodeCallback = code => {
+            console.log('barcodeCallback');
+
+            // Commands are 5 characters long.
+            if (code.length <= 5) {
+                if (code === '03006') {
+                    handleReset();
+                }
+                return;
+            }
+
+            actionHandler('borrowMaterial', {
+                itemIdentifier: code
+            });
+        };
+
+        barcodeScanner.start(barcodeCallback);
+        return () => {
+            barcodeScanner.stop();
+        };
+    }, [actionHandler, handleReset]);
+
+    // Return nothing if no machineState is set.
+    if (!Object.prototype.hasOwnProperty.call(props, 'machineState')) {
+        return;
+    }
+
+    return (
+        <Container>
+            <h1>Return</h1>
+
+            <Row>
+                <Col>
+                    <Alert variant={'info'}>Skan materialer</Alert>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h2>Materials</h2>
+
+                    <Table striped={true} bordered={true}>
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>title</th>
+                            <th>author</th>
+                            <th>status</th>
+                            <th>message</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            props.machineState.materials && props.machineState.materials.map(
+                                el => <tr key={'material-' + el.itemIdentifier}>
+                                    <td>{el.itemIdentifier}</td>
+                                    <td>{el.title}</td>
+                                    <td>{el.author}</td>
+                                    <td>{el.status}</td>
+                                    <td>{el.message}</td>
+                                </tr>
+                            )
+                        }
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button variant={'primary'} onClick={props.handleReset}>
+                        Tilbage
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+Return.propTypes = {
+    actionHandler: PropTypes.func.isRequired,
+    handleReset: PropTypes.func.isRequired,
+    machineState: PropTypes.object.isRequired
+};
+
+export default Return;
