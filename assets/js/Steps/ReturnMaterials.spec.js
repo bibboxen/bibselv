900850208c492fb0e58b8c3877f5ead18e158671
Troels Ugilt Jensen <tujensen@gmail.com>
2020-07-29 12:58:48 +0200

    BIB-390: Fixed import

--- assets/js/Steps/ReturnMaterials.spec.js
+++ assets/js/Steps/ReturnMaterials.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { it } from '@jest/globals';
-import ReturnMaterials from './Return';
+import ReturnMaterials from './ReturnMaterials';
 
 it('renders without crashing', () => {
     const machineState = {
