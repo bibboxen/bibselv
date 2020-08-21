8fe7c9ef33c03d5b2d03c194fde49b7ed9158aff
Ture Gjørup <ture@ignatz.dk>
2020-08-21 09:12:57 +0200

    BIB-444: Re-added repository methods

---
+++ src/Repository/Sip2UserRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Sip2User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Sip2User|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Sip2User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Sip2User[]    findAll()
+ * @method Sip2User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class Sip2UserRepository extends ServiceEntityRepository
+{
+    /**
+     * Sip2UserRepository constructor.
+     *
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Sip2User::class);
+    }
+
+    // /**
+    //  * @return Sip2User[] Returns an array of Sip2User objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Sip2User
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
