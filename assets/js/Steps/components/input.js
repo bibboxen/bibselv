0bee7d8fab1c09c801d5b17e3293929a893a625e
Sine Jespersen <sinejespersen@Sines-MacBook-Pro-2.local>
2020-09-16 07:59:55 +0200

    BIB-522: readded readonly as prop

--- assets/js/Steps/components/input.js
+++ assets/js/Steps/components/input.js
@@ -23,7 +23,7 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
  * @return {*}
  * @constructor
  */
-const Input = ({ name, label, value, which }) => {
+const Input = ({ name, label, value, which, ...rest }) => {
     let cssClass = 'input';
     if (which && value) {
         cssClass =
@@ -34,7 +34,7 @@ const Input = ({ name, label, value, which }) => {
     return (
         <div className={cssClass}>
             <label htmlFor={name}>{label}</label>
-            <input value={value} name={name} id={name} type='text' readOnly />
+            <input value={value} name={name} id={name} type='text' {...rest} />
             {value && which && (
                 <div className='info-banner'>
                     <span className='info-banner-icon'>
