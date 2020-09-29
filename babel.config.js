44e71b7f41626b7996abdb7692d94eeb70837600
Jesper Kristensen <cableman@linuxdev.dk>
2020-09-29 16:21:33 +0200

    BIB-547: Added compiled json language files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: [
+        [
+            "react-intl",
+            {
+                "extractFromFormatMessageCall": true,
+                "idInterpolationPattern": "[sha512:contenthash:base64:6]",
+                "ast": true
+            }
+        ]
+    ]
 };
