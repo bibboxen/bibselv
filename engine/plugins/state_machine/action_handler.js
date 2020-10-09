23cc7aa125f55048d0812f7521d6d482e1bc04db
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2020-10-09 08:53:49 +0200

    BIB-569: Fixed issues raised in review

--- engine/plugins/state_machine/action_handler.js
+++ engine/plugins/state_machine/action_handler.js
@@ -76,11 +76,11 @@ class ActionHandler {
     /**
      * Check out item for the client.
      *
-     * @param client
+     * @param {object} client
      *   The client.
      */
     checkOutItem(client) {
-        const newItem = this.getItem(client);
+        const newItem = this.filterIdentifier(client.actionData);
 
         // Ignore item if it is already checkedOut or inProgress.
         // @TODO: Handle retry case.
@@ -166,11 +166,11 @@ class ActionHandler {
     /**
      * Check in item for the client.
      *
-     * @param client
+     * @param {object} client
      *   The client.
      */
     checkInItem(client) {
-        const newItem = this.getItem(client);
+        const newItem = this.filterIdentifier(client.actionData);
 
         // Ignore item if it is already checkedIn or inProgress.
         // @TODO: Handle retry case.
@@ -250,33 +250,32 @@ class ActionHandler {
     }
 
     /**
-     * Get item from client.actionData.
+     * Filters the itemIdentifier from client.actionData.
      *
      * Applies 2of5 transformation of itemIdentifier.
      *
-     * @param client
+     * @param {object} item
+     *   The item to filter
      *
-     * @returns {null}
+     * @returns {object}
      */
-    getItem(client) {
-        const newItem = client.actionData;
-
+    filterIdentifier(item) {
         // Handle itemIdentifiers that are coded (because of Barcode scanner).
         const pattern = /^(!B[A-Z]\d{2})(?<code>.+)!C$/;
-        const matches = pattern.exec(newItem.itemIdentifier);
+        const matches = pattern.exec(item.itemIdentifier);
 
         if (matches !== null && matches.groups.code) {
             let outputItemIdentifier = matches.groups.code;
 
             // Prefix 2of5 barcodes with "LL-".
-            if (newItem.itemIdentifier.substr(1, 4) === BARCODE_CODE_2OF5) {
+            if (item.itemIdentifier.substr(1, 4) === BARCODE_CODE_2OF5) {
                 outputItemIdentifier = PREFIX_2OF5 + outputItemIdentifier;
             }
 
-            newItem.itemIdentifier = outputItemIdentifier;
+            item.itemIdentifier = outputItemIdentifier;
         }
 
-        return newItem;
+        return item;
     }
 
     /**
