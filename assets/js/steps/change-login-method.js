bcd05dbe9b231396a800295cea6e752363652b9b
Sine <sinje@aarhus.dk>
2021-05-26 10:39:29 +0200

    bib-622: styled change-login-method

--- assets/js/steps/change-login-method.js
+++ assets/js/steps/change-login-method.js
@@ -6,9 +6,17 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Bubble from './components/bubble';
-import { faBarcode } from '@fortawesome/free-solid-svg-icons';
-import { ChangeLoginMethodScanUsername, ChangeLoginMethodScanUsernameAndPassword } from './utils/formatted-messages';
+import HelpBox from './components/help-box';
+import { faBarcode, faLock, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+    ChangeLoginMethodScanUsername,
+    ChangeLoginMethodScanUsernameAndPassword,
+    ChangeLoginMethodPickLoginMethodHeader,
+    ChangeLoginMethodPickLoginMethodSubheader,
+    ChangeLoginMethodTimeoutMessage
+} from './utils/formatted-messages';
 import MachineStateContext from './utils/machine-state-context';
+import Header from './components/header';
 
 /**
  * Change login method.
@@ -20,19 +28,18 @@ import MachineStateContext from './utils/machine-state-context';
  */
 function ChangeLoginMethod({ actionHandler }) {
     const context = useContext(MachineStateContext);
-    const boxConfig = context.boxConfig;
-
+    const { loginSessionMethods, loginSessionTimeout } = context.boxConfig.get;
     const components = [];
 
-    if (boxConfig?.get?.loginSessionMethods?.includes('login_barcode_password')) {
+    if (loginSessionMethods.includes('login_barcode_password')) {
         components.push({
             type: 'loginScanUsernamePassword',
             label: ChangeLoginMethodScanUsernameAndPassword,
-            icon: faBarcode
+            icon: faLock
         });
     }
 
-    if (boxConfig?.get?.loginSessionMethods?.includes('login_barcode')) {
+    if (loginSessionMethods.includes('login_barcode')) {
         components.push({
             type: 'loginScanUsername',
             label: ChangeLoginMethodScanUsername,
@@ -52,32 +59,36 @@ function ChangeLoginMethod({ actionHandler }) {
     }
 
     return (
-        <div className='col-md-12'>
-            <h1 className='mb-5'>
-                {'Vælg login metode'}
-            </h1>
-            <div>
-                { /* @TODO: Fix translation and styling */ }
-                <span>Timeout: </span> {boxConfig?.get?.loginSessionTimeout} s.
-                Efter denne periode stopper login sessionen.
+        <>
+            <Header
+                header={ChangeLoginMethodPickLoginMethodHeader}
+                subheader={ChangeLoginMethodPickLoginMethodSubheader}
+                type='login'
+                icon={faSignInAlt}
+            />
+            <div className='col-md-3'>
+                <HelpBox text={ChangeLoginMethodTimeoutMessage(loginSessionTimeout)} />
             </div>
-            <div className='row justify-content-center'>
-                {components.map((component) => (
-                    <div key={component.type} className='col-md-3'>
-                        <Bubble
-                            type={component.type}
-                            label={component.label}
-                            icon={component.icon}
-                            img={component.img}
-                            disabled={component.disabled}
-                            onClick={() => {
-                                handleChangeLoginMethod(component.type);
-                            }}
-                        />
-                    </div>
-                ))}
+            <div className="col-md-1" />
+            <div className='col-md-12'>
+                <div className='row justify-content-center'>
+                    {components.map((component) => (
+                        <div key={component.type} className='col-md-3'>
+                            <Bubble
+                                type={component.type}
+                                label={component.label}
+                                icon={component.icon}
+                                img={component.img}
+                                disabled={component.disabled}
+                                onClick={() => {
+                                    handleChangeLoginMethod(component.type);
+                                }}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
