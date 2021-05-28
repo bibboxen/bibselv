beb339baec65e3142d42bf19bb13b65675c8447a
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2021-05-28 10:44:23 +0200

    BIBSELV-212: Changed default value for field

--- src/Entity/BoxConfiguration.php
+++ src/Entity/BoxConfiguration.php
@@ -122,11 +122,11 @@ class BoxConfiguration
     private $uniqueId;
 
     /**
-     * @ORM\Column(type="boolean", nullable=true)
+     * @ORM\Column(type="boolean", nullable=true, options={"default":"1"})
      *
      * @Groups("boxConfiguration")
      */
-    private $hasFrontpageCheckIn;
+    private $hasFrontpageCheckIn = true;
 
     /**
      * BoxConfiguration toString.
