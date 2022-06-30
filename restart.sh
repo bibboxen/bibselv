25897ccbd40d37962b2eff926bdb6ea6af72d787
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2022-06-30 16:33:51 +0200

    SERV-546: Added to restart script

--- restart.sh
+++ restart.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+# Pull and up containers
 docker-compose --env-file .env.docker.local -f docker-compose.server.yml pull
 docker-compose --env-file .env.docker.local -f docker-compose.server.yml up --force-recreate --detach --remove-orphans
 
+# Install frontend packages
+docker-compose --env-file .env.docker.local -f docker-compose.server.yml exec frontend bash -c 'npm install'
+
+# Install engine packages
 docker-compose --env-file .env.docker.local -f docker-compose.server.yml exec engine bash -c './scripts/install.sh'
 
-docker-compose --env-file .env.docker.local -f docker-compose.server.yml restart
+# Install php bundles
+docker-compose --env-file .env.docker.local -f docker-compose.server.yml exec --user deploy phpfpm composer install --no-dev -o
 
+# Migrate database
 docker-compose --env-file .env.docker.local -f docker-compose.server.yml exec --user deploy phpfpm bin/console doctrine:migrations:migrate --no-interaction
 
-# frontend assets will be rebuilt automatically
+# Restart containers
+# Frontend assets will be rebuilt automatically when the container is restarted
+docker-compose --env-file .env.docker.local -f docker-compose.server.yml restart
