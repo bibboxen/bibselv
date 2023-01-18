a9b5ed8ab1fc19faa8a9ef2cc6aa3ac0fa6c749f
Ture Gjørup <ture@ignatz.dk>
2023-01-18 12:52:44 +0100

    BIBSELV-266: Fixes to restart script

--- restart.sh
+++ restart.sh
@@ -8,12 +8,13 @@ composer install --no-dev -o
 
 # Run database migrations
 echo
-bin/console doctrine:migrations:migrate
+bin/console doctrine:migrations:migrate --no-interaction
 
 # Build frontend packages
 echo
 echo "NPM version:" "$(npm -v)" "/ Node version:" "$(node -v)"
 echo
+npm install --omit=dev
 npm run build
 
 # Install engine packages
