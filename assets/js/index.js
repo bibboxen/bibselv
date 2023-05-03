851a27d8556276745ea95ff914bee4c39a010ea0
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2023-05-03 11:16:15 +0200

    SUPPORT-991: Removed worker

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,28 +12,11 @@ const rootElement = document.getElementById('reactjs-root');
 const socket = socketIOClient(rootElement.getAttribute('data-socket-uri'));
 const root = createRoot(rootElement);
 
-root.render(<React.StrictMode>
-    <App
-        uniqueId={rootElement.getAttribute('data-unique-id')}
-        socket={socket}
-    />
-</React.StrictMode>);
-
-// Initialize web worker to check for unresponsive frontend.
-// If it is unresponsive, reload the page.
-if (window.Worker) {
-    const myWorker = new Worker('../worker.js');
-
-    myWorker.onmessage = function(e) {
-        switch (e?.data) {
-            case 'reload':
-                window.location.reload();
-                break;
-            case 'ping':
-                myWorker.postMessage('pong');
-                break;
-        }
-    };
-} else {
-    console.warn('Your browser does not support web workers.');
-}
+root.render(
+    <React.StrictMode>
+        <App
+            uniqueId={rootElement.getAttribute('data-unique-id')}
+            socket={socket}
+        />
+    </React.StrictMode>
+);
