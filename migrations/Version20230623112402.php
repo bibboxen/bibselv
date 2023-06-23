ee0ca6fe64ab9edb4a758dfee9522f1ae0a2a3f4
Sine Jespersen <sinejespersen@gmail.com>
2023-06-23 13:24:34 +0200

    BIBSELV-279: add to migrations

---
+++ migrations/Version20230623112402.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230623112402 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE box_configuration ADD other_permanent_location_instruction VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE box_configuration DROP other_permanent_location_instruction');
+    }
+}
