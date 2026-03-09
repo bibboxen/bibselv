05e9a9221a4e528c3d5c4ffe527d5edc342b8952
Troels Ugilt Jensen <6103205+tuj@users.noreply.github.com>
2026-03-09 10:02:59 +0100

    6885: Npm audit fixes

---
+++ engine/scripts/audit-fix.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")/.." || exit
+
+# Audit main modules
+echo '-- Audit main modules --'
+echo "==> ."
+rm -rf node_modules
+npm install --${1:-omit=dev}
+npm audit --omit=dev fix
+echo '-----------------------'
+
+# Audit plugin dependencies.
+echo
+echo '-- Audit plugin dependencies --'
+for folder in plugins/*; do
+  if [ -d $folder ]; then
+    echo
+    echo '-----------------------'
+    echo "==> ${folder}"
+    echo '-----------------------'
+    cd $folder
+    rm -rf node_modules
+    npm install --${1:-omit=dev}
+    npm audit --omit=dev fix
+    cd ../..
+    echo '-----------------------'
+  fi
+done
